29a86054724a283a749029acdf3dddf95c8c9643
Howard Wen <wen.howard2012@gmail.com>
2021-08-20 14:48:24 -0600

    permanent riot api key

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 import datetime
 import time
 
-riotAPI = input("Enter riotAPI key: ")
+riotAPI = "RGAPI-ea484409-9ff3-4f05-977e-7e1f815e3ba5"
 
 summonerName = input("Enter Summoner Name: ")
 summonerName = summonerName.replace(" ", "%20")
 
-timeLength = input("Enter time length: ")
+timeLength = input("Enter time length in days: ")
 timeLengthMil = int(int(timeLength)*86400000)
 currentTime = time.time() * 1000
 timeFilter = currentTime - timeLengthMil
